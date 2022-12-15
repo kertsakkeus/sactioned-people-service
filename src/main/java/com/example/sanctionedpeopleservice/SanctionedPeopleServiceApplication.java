91cd84c39632cd52adb939f04a36e0c1befe02b0
Kert Sakkeus <kertsakkeus@gmail.com>
2022-12-15 21:56:04 +0200

    N-1512-4: add swagger support

--- src/main/java/com/example/sanctionedpeopleservice/SanctionedPeopleServiceApplication.java
+++ src/main/java/com/example/sanctionedpeopleservice/SanctionedPeopleServiceApplication.java
@@ -1,8 +1,15 @@
 package com.example.sanctionedpeopleservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(
+    title = "Sanctioned People API",
+    version = "1.0",
+    description = "Service that compares a given name against a list of sanctioned people to detect suspicious transfers."
+))
 @SpringBootApplication
 public class SanctionedPeopleServiceApplication {
   public static void main(String[] args) {
